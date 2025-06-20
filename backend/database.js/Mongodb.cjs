14590bbe1fdecff1bb1d030425f75a4f5a6cc7bc
Sunicreates <Sunilkumarerere@gmail.com>
2025-06-20 11:40:59 -0700

    Update Mongodb.cjs

--- backend/database.js/Mongodb.cjs
+++ backend/database.js/Mongodb.cjs
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'https://your-vercel-app.vercel.app',
+    'https://fashionindeploy.vercel.app',
     'http://localhost:3000'
   ],
   credentials: true
